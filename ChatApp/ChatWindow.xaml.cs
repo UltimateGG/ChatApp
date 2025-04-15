fa09557f5eea726a40b663cd64ace038fcf47980
Trent <trentosgood10@gmail.com>
2025-04-14 20:50:54 -0500

    implemented hostServer, connectToServer, and sendMessage functions.

--- ChatApp/ChatWindow.xaml.cs
+++ ChatApp/ChatWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System.Diagnostics;
-using System.Text.RegularExpressions;
-using System.Windows.Input;
-using System.Windows;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace ChatApp;
@@ -15,24 +18,116 @@ public partial class ChatWindow : Window
     Boolean hosting;
     int messageCount = 0;
     Semaphore mutex; //Semaphore used to keep race conditions from happening
-    String senderName = "Sender"; //Name of the other person. Defaults to sender, but could be set the ip:portnumber of the other person when the connection is made
+    String senderName = "Sender"; //Name of the other person.
 
     //Function to create a server host
     public void hostServer()
     {
-        return;
+        Task.Run(() =>
+        {
+            try
+            {
+                int portNum = int.Parse(this.port);
+                IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, portNum);
+                Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+                Console.WriteLine("Waiting for a connection on port " + portNum);
+                // This blocking call runs on a background thread.
+                Socket client = listener.Accept();
+                Console.WriteLine("Client connected.");
+                // Store the connection socket in the Tag property.
+                Dispatcher.Invoke(() => { this.Tag = client; });
+                // Start receiving messages on this connection.
+                StartReceiving(client);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error hosting server: " + ex.Message);
+            }
+        });
     }
 
-    //Function to connect to an exiting host
+    //Function to connect to an existing host
     public void connectToServer()
     {
-        return;
+        Task.Run(() =>
+        {
+            try
+            {
+                if (!IPAddress.TryParse(this.ip, out IPAddress remoteIP))
+                {
+                    Console.WriteLine("Invalid IP address.");
+                    return;
+                }
+                int portNum = int.Parse(this.port);
+                IPEndPoint remoteEP = new IPEndPoint(remoteIP, portNum);
+                Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Console.WriteLine("Connecting to server at " + this.ip + ":" + portNum);
+                clientSocket.Connect(remoteEP);
+                Console.WriteLine("Connected to server.");
+                Dispatcher.Invoke(() => { this.Tag = clientSocket; });
+                // Start receiving messages on this connection.
+                StartReceiving(clientSocket);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error connecting to server: " + ex.Message);
+            }
+        });
     }
 
-    //Function to send a message to other person
+    //Function to send a message to the other person
     public void sendMessage(String message)
     {
-        return;
+        try
+        {
+            if (this.Tag is Socket connectionSocket && connectionSocket.Connected)
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                connectionSocket.Send(data);
+            }
+            else
+            {
+                Console.WriteLine("No valid connection to send message.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error sending message: " + ex.Message);
+        }
+    }
+
+    //Helper method to start receiving messages in a background task.
+    private void StartReceiving(Socket connection)
+    {
+        Task.Run(() =>
+        {
+            byte[] buffer = new byte[1024];
+            try
+            {
+                while (true)
+                {
+                    int bytesRead = connection.Receive(buffer);
+                    if (bytesRead <= 0)
+                    {
+                        // Connection closed.
+                        break;
+                    }
+                    string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    // Update UI on the dispatcher thread.
+                    Dispatcher.Invoke(() => { addRecievedMessage(receivedMessage); });
+                }
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("Socket error receiving message: " + se.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error receiving message: " + ex.Message);
+            }
+        });
     }
 
     //Function to add a sent message to the screen
@@ -42,7 +137,7 @@ public partial class ChatWindow : Window
         mutex.WaitOne();
         Console.WriteLine("In semaphore.");
         this.messageCount++;
-        
+
         //Add two row definitions to the chat messages grid
         RowDefinition rowDefinition = new RowDefinition();
         rowDefinition.Height = GridLength.Auto;
@@ -82,7 +177,7 @@ public partial class ChatWindow : Window
         Console.WriteLine("Exiting semaphore.");
     }
 
-    //Function to add a recieved message to the screen
+    //Function to add a received message to the screen
     public void addRecievedMessage(String message)
     {
         mutex.WaitOne();
@@ -129,20 +224,21 @@ public partial class ChatWindow : Window
     public ChatWindow(String ip, String port, Boolean hosting)
     {
         Console.WriteLine("Constructor Entry");
+        InitializeComponent();
         this.ip = ip;
         this.port = port;
         this.hosting = hosting;
         this.mutex = new Semaphore(initialCount: 1, maximumCount: 2);
 
-        if(this.hosting)
+        if (this.hosting)
         {
             hostServer();
-        } else
+        }
+        else
         {
             connectToServer();
         }
 
-        InitializeComponent();
         Console.WriteLine("Constructor Successful");
     }
 
